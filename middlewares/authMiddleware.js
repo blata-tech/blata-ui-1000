0b94385fccde201ac38bf97e830c201a50861d7f
Tewodros Gerem <tedy.admass@gmail.com>
2025-08-20 12:36:19 +0100

    The backend of a medicine store application has been updated with new middleware for authentication and authorization. The changes include:

--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -6,11 +6,18 @@ export const authMiddleware = (req, res, next) => {
             res.status(401).json({message: 'no token provided'});
         }
         const decoded = jwt.verify(token, process.env.JWT_SECRET);
-        
-
+        req.user = decoded;
+        next();
 
     }catch(err) {
+        res.status(401).json({error: 'unauthrized access'})
 
     }
 
+};
+export const adminOnly = (req, res, next) => {
+    if(req.user.role !== 'admin') {
+        return res.status(403).json({message: 'access forbidden: admin only'});
+    }
+    next();
 };
